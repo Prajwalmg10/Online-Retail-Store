ae1542cc78e0d236349a486ea1707a9ea79537f6
Prajwalmg10 <121098209+Prajwalmg10@users.noreply.github.com>
2024-06-30 23:41:18 +0530

    inventory impl

--- src/main/java/com/example/retailStore/Services/Implementation/InventoryServiceImpl.java
+++ src/main/java/com/example/retailStore/Services/Implementation/InventoryServiceImpl.java
@@ -3,7 +3,11 @@ package com.example.retailStore.Services.Implementation;
 import com.example.retailStore.Models.Inventory;
 import com.example.retailStore.Services.InventoryServices;
 import com.example.retailStore.uiResponse.DataException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class InventoryServiceImpl implements InventoryServices {
     @Override
     public Inventory addToInventory(Inventory inventory) throws DataException {
